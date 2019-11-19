941dab329f32e8e033a4557fcd3d0bd43f754fbd
ces518 <40373936+ces518@users.noreply.github.com>
2019-11-19 22:55:18 +0900

    리액트 3주차
    리액트 3주차

--- react/todo-app/Todo.js
+++ react/todo-app/Todo.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Todo.css';
 
-const Todo = ({ text }) => {
+const Todo = ({ id, text, checked, onToggle }) => {
     return (
-        <div className="todo-item">
+        <div className="todo-item" onClick={onToggle(id)}>
             <div className="remove" >&times;</div>
-            <div className="todo-text">
+            {/* 만약 Todo 할일이 완료되었다면 (checked = true 상태) 체크표시 */}
+            <div className={`todo-text ${checked && 'checked'}`}>
             <div>{text}</div>
             </div>
         </div>        
