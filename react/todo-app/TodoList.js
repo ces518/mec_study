55d373baf088ffdabe454a145885a255b14ca300
ces518 <40373936+ces518@users.noreply.github.com>
2019-11-27 13:37:44 +0900

    리액트 4주차

--- react/todo-app/TodoList.js
+++ react/todo-app/TodoList.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import Todo from './Todo';
 
-const TodoList = ({ todos, onToggle }) => {
+const TodoList = ({ todos, onToggle, onRemove }) => {
     // 부모로 부터 받은 todos props를 Todo 컴포넌트 목록 으로 변환하여, 랜더링 해준다.
     return (
         <>
             {
                 todos.map(
-                    ({ id, text, checked }) => 
+                    ({ id, text, checked, color }) => 
                         (<Todo 
                             id={id}
                             text={text}
                             checked={checked}
                             onToggle={onToggle}
+                            onRemove={onRemove}
+                            color={color}
                             key={id}
                         />)
                 )
