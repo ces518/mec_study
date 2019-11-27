d89dedf24f18c3071a2d7887ce0dee5429fa4f79
ces518 <40373936+ces518@users.noreply.github.com>
2019-11-27 13:37:59 +0900

    리액트 4주차

--- react/todo-app/Todo.js
+++ react/todo-app/Todo.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Todo.css';
 
-const Todo = ({ id, text, checked, onToggle }) => {
+const Todo = ({ id, text, checked, onToggle, onRemove, color }) => {
     return (
-        <div className="todo-item" onClick={onToggle(id)}>
-            <div className="remove" >&times;</div>
+        <div className="todo-item">
+            {/* 삭제 이벤트 바인딩 */}
+            <div className="remove" onClick={onRemove(id)}>&times;</div>
             {/* 만약 Todo 할일이 완료되었다면 (checked = true 상태) 체크표시 */}
-            <div className={`todo-text ${checked && 'checked'}`}>
-            <div>{text}</div>
+            <div className={`todo-text ${checked && 'checked'}`} onClick={onToggle(id)}>
+            <div style={{ color }}>{text}</div>
             </div>
         </div>        
     );
